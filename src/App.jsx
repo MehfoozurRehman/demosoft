bbd2a6919f33778b5dac907e0f93efb07b96ca7b
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-05-26 10:33:52 +0500

    base done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,92 @@
 import React from "react";
+import "./App.css";
 
 export default function App() {
-  return <div>App</div>;
+  const blogs = [
+    { heading: "hello", info: "helookjfljsadlkjfl" },
+    { heading: "hello", info: "helookjfljsadlkjfl" },
+  ];
+  return (
+    <div className="card__wrapper">
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+        exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      <Card
+        heading="Lorem, ipsum dolor"
+        info="exercitationem facilis modi omnis at odit harum, animi ab iste sunt."
+      />
+      {blogs.map(({ heading, info }) => {
+        return (
+          <a href="#" className="card">
+            <div className="card__heading">{heading}</div>
+            <div className="card__info">{info}</div>
+          </a>
+        );
+      })}
+      <Navbar />
+    </div>
+  );
+}
+
+function Card({ heading, info }) {
+  return (
+    <a href="#" className="card">
+      <div className="card__heading">{heading}</div>
+      <div className="card__info">{info}</div>
+    </a>
+  );
+}
+
+function Navbar() {
+  return <div>sldkfja;lsdkjflk;dsaj;flkjsal;dkjfklsadjflkjsadf;klj</div>;
 }
